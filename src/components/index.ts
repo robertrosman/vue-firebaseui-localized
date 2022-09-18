0dfdcc6979358044111d1e6bfc444ab5b2d9bead
Robert Rosman <robert.rosman@zmartagroup.com>
2022-09-18 15:46:04 +0200

    Export all components to library

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as VueFirebaseuiLocalized } from './VueFirebaseuiLocalized.vue'
+export { default as LoadingContainer } from './LoadingContainer.vue'
+export { default as SkeletonButton } from './SkeletonButton.vue'
